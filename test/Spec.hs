9adbb01a45719ff610c15c378f454172ec1d9f8b
Cameron Angliss <cameronangliss99@gmail.com>
2022-11-15 12:36:27 -0500

    debugging program

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,17 @@
+module Spec where
+
+import           BotGA                          ( BotRecords(maxFs)
+                                                , getInitBotRecords
+                                                , runBotGA
+                                                )
+import           Control.Monad.Random           ( mkStdGen
+                                                , runRand
+                                                )
+import           NetGA                          ( Params(..) )
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    let params = Params { numNets = 100, numNrns = 5, numTests = 1, i = 100, mut = 1.0e-3, elitism = True, s = 1 }
+        (records, g) = runRand (getInitBotRecords params) (mkStdGen $ s params)
+        (nextRecords, _) = runRand (runBotGA params records) g
+    print (last $ maxFs nextRecords)
