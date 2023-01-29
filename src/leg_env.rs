86d53281c00b652c43cfd84dd8701a2ca0555f40
Cameron Angliss <cameronangliss99@gmail.com>
2023-01-29 03:04:16 -0500

    refactor

--- src/leg_env.rs
+++ src/leg_env.rs
@@ -24,8 +24,8 @@ impl Default for LegEnv {
 
 impl LegEnv {
     pub fn act(&mut self, x_signal: usize, y_signal: usize) {
-        self.calc_momentum(x_signal, y_signal);
-        self.calc_position(x_signal, y_signal);
+        self.update_momentum(x_signal, y_signal);
+        self.update_position(x_signal, y_signal);
         self.sense();
     }
 
@@ -36,63 +36,40 @@ impl LegEnv {
         self.sens_values = [sens_value1, sens_value2, sens_value3];
     }
 
-    fn calc_momentum(&mut self, x_signal: usize, y_signal: usize) {
-        self.calc_x_mom(x_signal);
-        self.calc_y_mom(y_signal);
+    fn update_momentum(&mut self, x_signal: usize, y_signal: usize) {
+        self.x_mom = calc_mom(x_signal, XS, self.x_pos, self.x_mom);
+        self.y_mom = calc_mom(y_signal, YS, self.y_pos, self.y_mom);
     }
 
-    fn calc_x_mom(&mut self, signal: usize) {
-        let request = XS[signal];
-        let delta = request - self.x_pos;
-        self.x_mom = match self.x_mom {
-            x_mom if x_mom.signum() != delta.signum() => delta.signum(),
-            _ if delta == 0 => 0,
-            x_mom if x_mom.abs() == 3 => x_mom,
-            x_mom if x_mom > 0 => x_mom + 1,
-            _ => self.x_mom - 1,
-        };
+    fn update_position(&mut self, x_signal: usize, y_signal: usize) {
+        self.x_pos = calc_pos(x_signal, XS, self.x_pos, self.x_mom, 10.0);
+        self.y_pos = calc_pos(y_signal, YS, self.y_pos, self.y_mom, 5.0);
     }
+}
 
-    fn calc_y_mom(&mut self, signal: usize) {
-        let request = YS[signal];
-        let delta = request - self.y_pos;
-        self.y_mom = match self.y_mom {
-            y_mom if y_mom.signum() != delta.signum() => delta.signum(),
-            _ if delta == 0 => 0,
-            y_mom if y_mom.abs() == 3 => y_mom,
-            y_mom if y_mom > 0 => y_mom + 1,
-            _ => self.y_mom - 1,
-        };
+fn calc_mom(signal: usize, pos_list: [i16; 16], pos: i16, mom: i16) -> i16 {
+    let request = pos_list[signal];
+    let delta = request - pos;
+    match mom {
+        mom if mom.signum() != delta.signum() => delta.signum(),
+        mom if mom.abs() == 3 => mom,
+        mom if mom > 0 => mom + 1,
+        mom if mom < 0 => mom - 1,
+        _ => 0,
     }
+}
 
-    fn calc_position(&mut self, x_signal: usize, y_signal: usize) {
-        self.calc_x_pos(x_signal);
-        self.calc_y_pos(y_signal);
-    }
-
-    fn calc_x_pos(&mut self, signal: usize) {
-        if self.x_mom != 0 {
-            let request = XS[signal];
-            let delta = request - self.x_pos;
-            let max_dist = (10.0 * 2.0_f32.powf(self.x_mom.abs() as f32 - 3.0)).floor() as i16;
-            if delta.abs() <= max_dist {
-                self.x_pos = request;
-            } else {
-                self.x_pos += delta.signum() * max_dist;
-            }
-        }
-    }
-
-    fn calc_y_pos(&mut self, signal: usize) {
-        if self.y_mom != 0 {
-            let request = YS[signal];
-            let delta = request - self.y_pos;
-            let max_dist = (5.0 * 2.0_f32.powf(self.y_mom.abs() as f32 - 3.0)).floor() as i16;
-            if delta.abs() <= max_dist {
-                self.y_pos = request;
-            } else {
-                self.y_pos += delta.signum() * max_dist;
-            }
+fn calc_pos(signal: usize, pos_list: [i16; 16], pos: i16, mom: i16, dist_limit: f32) -> i16 {
+    if mom == 0 {
+        pos
+    } else {
+        let request = pos_list[signal];
+        let delta = request - pos;
+        let max_dist = (dist_limit * 2.0_f32.powf(mom.abs() as f32 - 3.0)).floor() as i16;
+        if delta.abs() <= max_dist {
+            request
+        } else {
+            pos + delta.signum() * max_dist
         }
     }
 }
