7aa59c8d2ee5284c895fb741885aae7c227fd213
Cameron Angliss <cameronangliss99@gmail.com>
2023-02-04 17:29:30 -0500

    bug fixes and optimizations

--- src/bot_env.rs
+++ src/bot_env.rs
@@ -11,13 +11,16 @@ impl BotEnv {
     pub fn calc_path(&mut self, position_lists: &Vec<Vec<(i16, i16)>>) -> Vec<(f32, f32)> {
         let mut path = vec![(0.0, 0.0)];
         for i in 0..position_lists.len() - 1 {
-            let leg_heights: Vec<i16> = position_lists[i + 1].iter().map(|(_, y)| *y).collect();
+            let leg_heights = position_lists[i + 1]
+                .iter()
+                .map(|(_, y)| *y)
+                .collect::<Vec<_>>();
             let drag = get_drag_factor(&leg_heights);
-            let leg_velocities_rel_to_bot: Vec<i16> = position_lists[i]
+            let leg_velocities_rel_to_bot = position_lists[i]
                 .iter()
                 .zip(position_lists[i + 1].iter())
                 .map(|((x1, _), (x2, _))| x2 - x1)
-                .collect();
+                .collect::<Vec<_>>();
             let (thrust, deg_turn) = get_leg_effects(&leg_velocities_rel_to_bot, &leg_heights);
             self.dir += deg_turn;
             self.x_pos += drag * thrust * f32::cos(self.dir);
@@ -29,67 +32,46 @@ impl BotEnv {
 }
 
 fn get_leg_effects(leg_velocities_rel_to_bot: &[i16], leg_heights: &[i16]) -> (f32, f32) {
-    let leg_effectfulness_scores: Vec<f32> = leg_heights
+    let leg_effectfulness_scores = leg_heights
         .iter()
         .map(|&height| {
             if height < 5 {
                 let root = (29_f32.powf(0.5) - 5.0) / 2.0;
-                1.0 / (height as f32 + root) + root
+                1.0 / (height as f32 + root) - root
             } else {
                 0.0
             }
         })
-        .collect();
-    let left_scores: Vec<f32> = leg_effectfulness_scores
-        .iter()
-        .step_by(2)
-        .copied()
-        .collect();
-    let right_scores: Vec<f32> = leg_effectfulness_scores
-        .iter()
-        .skip(1)
-        .step_by(2)
-        .copied()
-        .collect();
-    let left_denom: f32 = left_scores.iter().sum();
-    let right_denom: f32 = right_scores.iter().sum();
-    let left_coeffs: Vec<f32> = left_scores
-        .iter()
-        .map(|&score| {
-            if left_denom == 0.0 {
-                0.0
-            } else {
-                score / left_denom
-            }
-        })
-        .collect();
-    let right_coeffs: Vec<f32> = right_scores
-        .iter()
-        .map(|&score| {
-            if right_denom == 0.0 {
-                0.0
-            } else {
-                score / right_denom
-            }
-        })
-        .collect();
+        .collect::<Vec<_>>();
+    let left_scores = leg_effectfulness_scores.iter().step_by(2).copied();
+    let right_scores = leg_effectfulness_scores.iter().skip(1).step_by(2).copied();
+    let left_denom: f32 = left_scores.clone().sum();
+    let right_denom: f32 = right_scores.clone().sum();
+    let left_coeffs = left_scores.map(|score| {
+        if left_denom == 0.0 {
+            0.0
+        } else {
+            score / left_denom
+        }
+    });
+    let right_coeffs = right_scores.map(|score| {
+        if right_denom == 0.0 {
+            0.0
+        } else {
+            score / right_denom
+        }
+    });
     let left_thrust = leg_velocities_rel_to_bot
         .iter()
         .step_by(2)
-        .zip(left_coeffs.iter())
-        .fold(0.0, |mut acc, (&velocity, &coeff)| {
-            acc += velocity as f32 * coeff;
-            acc
-        });
+        .zip(left_coeffs)
+        .fold(0.0, |acc, (&velocity, coeff)| acc + velocity as f32 * coeff);
     let right_thrust = leg_velocities_rel_to_bot
         .iter()
         .skip(1)
         .step_by(2)
-        .zip(right_coeffs.iter())
-        .fold(0.0, |mut acc, (&velocity, &coeff)| {
-            acc += velocity as f32 * coeff;
-            acc
-        });
+        .zip(right_coeffs)
+        .fold(0.0, |acc, (&velocity, coeff)| acc + velocity as f32 * coeff);
     let thrust = (left_thrust + right_thrust) / 2.0;
     let deg_turn = (right_thrust - left_thrust) / BOT_WIDTH;
     (thrust, deg_turn)
